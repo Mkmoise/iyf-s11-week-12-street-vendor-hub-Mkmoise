b746092694ab3f10ef071a4cbb10f2ffdf7b2c9c
Mkmoise <141993247+Mkmoise@users.noreply.github.com>
2026-08-21 13:36:47 +0000

    (dist) building client environment for production...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/iyf-s11-week-12-street-vendor-hub-Mkmoise/',
   plugins: [react()],
 })
