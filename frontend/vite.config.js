3fcd5260f44f1dacc52285f290954f0273bd8cb9
Mkmoise <mkmoise920@gmail.com>
2026-08-24 22:14:14 +0300

    Remove gh-pages deploy setup, clean up for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/iyf-s11-week-12-street-vendor-hub-Mkmoise/',
   plugins: [react()],
 })
