a8a5c71ecffe37ae916ec6f916748b6792d57faa
Moise Nyarugabo <mkmoise920@gmail.com>
2026-09-22 19:44:56 +0300

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,6 @@ import EditAdvertisement from "./pages/EditAdvertisement";
 import Posts from "./pages/Posts";
 import CreatePost from "./pages/CreatePost";
 import PostDetails from "./pages/PostDetails";
-import LandingPage from "./pages/LandingPage";
 
 import "./App.css";
 
